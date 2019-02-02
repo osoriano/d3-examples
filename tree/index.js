d75f0f333ed9dad58571e90b091360b091042125
osoriano <osoriano2@gmail.com>
2019-02-01 17:57:59 -0800

    Simplify tree build

---
+++ tree/index.js
@@ -0,0 +1,41 @@
+import Header from './header.js'
+import Tree from './tree.js'
+
+const EXAMPLE_PATH_INTERVAL = 1000
+const EXAMPLE_INPUT = [
+  ['2', '23', '234'],
+  ['1', '12', '123'],
+  ['3', '34', '345'],
+  ['2', '23', '235'],
+  ['2', '23', '235', '2356', '23567', '235678']
+]
+
+class Main {
+  constructor() {
+    this.tree = new Tree()
+    this.header = new Header(this.onInput.bind(this), this.onExampleData.bind(this))
+  }
+
+  onInput(input) {
+    return this.tree.addPath(input)
+  }
+
+  onExampleData() {
+    return new Promise(resolve => {
+      const previousShowNotifications = this.tree.showNotifications
+      this.tree.showNotifications = false
+
+      EXAMPLE_INPUT.forEach((input, i) => {
+        setTimeout(() => { this.tree.addPath(input) }, i * EXAMPLE_PATH_INTERVAL)
+      })
+
+      setTimeout(() => {
+        this.tree.showNotifications = previousShowNotifications
+        resolve()
+      }, (EXAMPLE_INPUT.length - 1) * EXAMPLE_PATH_INTERVAL)
+
+    })
+  }
+}
+
+new Main()
--- tree/tree.js
+++ tree/tree.js
@@ -1,5 +1,3 @@
-import './index.styl'
-
 const WIDTH = 2000
 const HEIGHT = 460
 const MARGIN_VERTICAL = 20
@@ -11,7 +9,7 @@ const DURATION = 750
 const DIAGONAL = d3.svg.diagonal().projection(d => [d.y, d.x])
 
 const TREE = d3.layout.tree().size([HEIGHT, WIDTH])
-    
+
 
 export default class Tree {
   constructor() {
@@ -77,7 +75,7 @@ export default class Tree {
     }
   }
 
-  addPath(path, cb) {
+  addPath(path) {
     let root = this.root
     /* The first node in the path that is not already part of the tree */
     let nodeToUpdate = null
@@ -97,72 +95,71 @@ export default class Tree {
     if (!nodeToUpdate) {
       const warning = 'Path already present'
       this.notification('warning', warning)
-      cb(warning)
-      return
+      return false
     }
     this.update(nodeToUpdate)
     this.notification('success', 'Added path')
-    cb()
+    return true
   }
 
   update(nodeToUpdate) {
     /* Compute the new tree layout */
     const nodes = TREE.nodes(this.root).reverse()
     const links = TREE.links(nodes)
-  
+
     /* Use fixed depth */
     nodes.forEach(d => { d.y = d.depth * 180 })
-  
+
     /* Assign id to node if not present */
     const node = this.svg.selectAll('g.node')
         .data(nodes, d => d.id || (d.id = ++this.nodeId))
-  
+
     /* New nodes enter at the parent's previous position */
     const nodeEnter = node.enter()
         .append('g')
         .attr('class', 'node')
         .attr('transform', d => 'translate(' + nodeToUpdate.y0 + ',' + nodeToUpdate.x0 + ')')
         .on('click', d => this.toggleChildren(d))
-  
+
     nodeEnter.append('circle')
         .attr('r', 1e-6)
         .style('fill', d => d._children ? 'lightsteelblue' : '#fff')
-  
+
     nodeEnter.append('text')
         .attr('x', d => d.children || d._children ? -13 : 13)
         .attr('dy', '.35em')
         .attr('text-anchor', d => d.children || d._children ? 'end' : 'start')
         .text(d => d.name)
         .style('fill-opacity', 1e-6)
-  
+
     /* Nodes then transition to their own positions */
     const nodeUpdate = node.transition()
         .duration(DURATION)
         .attr('transform', d => 'translate(' + d.y + ',' + d.x + ')')
-  
+
     nodeUpdate.select('circle')
         .attr('r', 10)
         .style('fill', d => d._children ? 'lightsteelblue' : '#fff');
-  
+
     nodeUpdate.select('text')
         .style('fill-opacity', 1);
-  
+
     /* Exiting nodes transition back to the parent's new position */
     const nodeExit = node.exit().transition()
         .duration(DURATION)
         .attr('transform', d => 'translate(' + nodeToUpdate.y + ',' + nodeToUpdate.x + ')')
         .remove()
-  
+
     nodeExit.select('circle')
         .attr('r', 1e-6);
-  
+
     nodeExit.select('text')
         .style('fill-opacity', 1e-6);
-  
+
     /* Update the links */
     const link = this.svg.selectAll('path.link')
         .data(links, d => d.target.id);
-  
+
     /* New links enter at the parent's previous position */
     link.enter().insert('path', 'g')
         .attr('class', 'link')
@@ -171,12 +168,12 @@ export default class Tree {
           const target = source;
           return DIAGONAL({source, target});
         });
-  
+
     /* Links then transition to their own position */
     link.transition()
         .duration(DURATION)
         .attr('d', DIAGONAL);
-  
+
     /* Exiting nodes go back to parent's new position */
     link.exit().transition()
         .duration(DURATION)
@@ -186,7 +183,7 @@ export default class Tree {
           return DIAGONAL({source, target});
         })
         .remove()
-  
+
     /* Stash the old positions for transition */
     nodes.forEach(d => {
       d.x0 = d.x;
