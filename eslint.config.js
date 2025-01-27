168dc942f7d164900a4bdb5b7e9cb5ca9679833b
Omar Soriano <osoriano2@gmail.com>
2025-01-26 20:37:39 -0800

    Bump dependencies
    Bump dependencies such as eslint and jest

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import pluginJest from 'eslint-plugin-jest'
+
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...pluginJest.environments.globals.globals,
+        d3: true,
+        dagre: true,
+      },
+    },
+    plugins: { jest: pluginJest },
+  },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+];
